86a4b0d07dfafcad8bcf814bec30ad6ce81aafc1
Quirin Manz <quirin.manz@tum.de>
2021-05-10 18:18:03 +0200

    configure bigtableparser.py and add EpiRR hashed

--- bigtableparser.py
+++ bigtableparser.py
@@ -26,12 +26,12 @@ def to_excel(fname, data, index=False):
 	return fname
 
 def hash_bigtable(f=None):
-	if not f: f = "raw/IHEC_metadata_summary.xlsx"
-	frame = pd.read_excel(f, sheet_name="BigTable")
+	if not f: f = "raw/EpiAtlas_EpiRR_metadata_all.csv" # IHEC_metadata_summary.xlsx"
+	frame = pd.read_csv(f) # read_excel(f, sheet_name="BigTable")
 	data = frame.to_dict()
 	columns = data.keys()
 	n = uniq({len(data[column]) for column in columns})
-	hashed = [{column: data[column][i] for column in columns} for i in range(n)] 
+	hashed = [{column: data[column][i] for column in columns} for i in range(n)]
 	jsoned = to_json(hashed) # clean up any unknown types
 	parsed = json.loads(jsoned)
 	return to_jsonfile(f + ".json", parsed)
@@ -39,29 +39,29 @@ def hash_bigtable(f=None):
 
 
 def merge_attributes(f=None):
-	if not f: f = "config/merges.json"
-	dbfile = 'raw/IHEC_metadata_summary.xlsx.json' 
-	cfg = from_jsonfile(f)
-	records = from_jsonfile(dbfile)
-	updated = list()
-	minimal = list()
-	for record in records:
-		sm_record = dict()
-		for rule in cfg:
-			strategy = uniq(rule['strategy']) 
-			opts = rule.get("options")
-			f = getattr(merging, strategy)
-			term = rule['harmonized']
-			record[term] = f(record, rule['strategy'][strategy], opts)
-			sm_record[term] = record[term]
-		updated.append(record)
-		minimal.append(sm_record)	
+    if not f: f = "config/merges.json"
+    dbfile = 'raw/EpiAtlas_EpiRR_metadata_all.csv.json'
+    cfg = from_jsonfile(f)
+    records = from_jsonfile(dbfile)
+    updated = list()
+    minimal = list()
+    for record in records:
+        sm_record = dict()
+        for rule in cfg:
+            strategy = uniq(rule['strategy'])
+            opts = rule.get("options")
+            f = getattr(merging, strategy)
+            term = rule['harmonized']
+            record[term] = f(record, rule['strategy'][strategy], opts)
+            sm_record[term] = record[term]
+        updated.append(record)
+        minimal.append(sm_record)
 
-	print(to_jsonfile('merged/IHEC_metadata.merged.json', updated))
-	print(to_jsonfile('merged/IHEC_metadata.merged_minimal.json', minimal))
-	print(to_excel('merged/IHEC_metadata.merged.xlsx', updated))
-	print(to_excel('merged/IHEC_metadata.merged_minimal.xlsx', minimal))
-	return 'ok'
+    print(to_jsonfile('merged/EpiAtlas_EpiRR_metadata_all.merged.json', updated))
+    print(to_jsonfile('merged/EpiAtlas_EpiRR_metadata_all.merged_minimal.json', minimal))
+    print(to_excel('merged/EpiAtlas_EpiRR_metadata_all.merged.xlsx', updated))
+    print(to_excel('merged/EpiAtlas_EpiRR_metadata_all.merged_minimal.xlsx', minimal))
+    return 'ok'
 
 if __name__ == "__main__":
 	cfg = sys.argv[1:]
