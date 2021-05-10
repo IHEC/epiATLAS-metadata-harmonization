493fbc4bd5cb943fcc33b3a4d872b72ec0f4187d
Quirin Manz <quirin.manz@tum.de>
2021-05-10 18:30:50 +0200

    adding more columns to merges.json and configuring merge_ontology option; adding merged EpiRR files

--- merging.py
+++ merging.py
@@ -1,5 +1,7 @@
 import math
+import re
 
+curie_re = re.compile(r'([A-Za-z]+)[_:](\d+)$')
 
 
 def uniq(es):
@@ -16,6 +18,14 @@ def try_int(e):
 	return x
 
 
+def try_curie(e):
+    m = re.search(curie_re, e)
+    if m:
+        return ':'.join([m.group(1).lower(), m.group(2)])
+    else:
+        return e
+
+
 def merge_delimited(record, cfg, opts=None):
 	def clean(e):
 		if isinstance(e, float):
@@ -27,13 +37,15 @@ def merge_delimited(record, cfg, opts=None):
 	found = [clean(record.get(e, '')) for e in cfg]
 	for e in found:
 		assert e.find(delimit) == -1
-	
+
 
 	cleanedup = {e for e in found if e}
-	if 'lowercase' in opts:	
+	if 'lowercase' in opts:
 		cleanedup = {e.lower() for e in cleanedup}
 	elif 'make_int' in opts:
 		cleanedup = {try_int(e) for e in cleanedup}
+	elif 'merge_ontology' in opts:
+		cleanedup = {try_curie(e) for e in cleanedup for e in e.split(';')}
 
 
 	if len(cleanedup) == 1: return uniq(cleanedup)
@@ -41,4 +53,4 @@ def merge_delimited(record, cfg, opts=None):
 		return delimit.join(cleanedup) if cleanedup else ''
 
 
-	
+
