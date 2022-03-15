41da2d492c88f8dbc786cdf33b2c57c090abd71d
Quirin Manz <quirin.manz@tum.de>
2022-03-15 18:34:57 +0100

    version 0.7

---
+++ openrefine/v0.7/create_v0.7.py
@@ -0,0 +1,28 @@
+import difflib
+import json
+import os.path
+import typing
+from subprocess import run
+
+import pandas as pd
+from pronto import Ontology
+
+# make sure the working directory when running this file is the project root of the git project
+os.chdir('../../')
+
+# create openrefine project and apply rules - OPENREFINE SERVER HAS TO BE RUNNING
+# creating openrefine projects via the openrefine-client needs a csv as input in order to work properly
+openrefine_client = './openrefine/openrefine-client_0-3-10_linux'  # path to the openrefine executable
+initial_csv = './openrefine/v0.6/IHEC_metadata_harmonization.v0.6.csv'  # csv to build project from
+
+old_project_name = os.path.splitext(os.path.basename(initial_csv))[0]
+
+# create project with old version
+run([openrefine_client, '--create', initial_csv], check=True)
+
+# apply rules
+run([openrefine_client, '--apply', 'openrefine/v0.7/solving_DEEP.json', old_project_name], check=True)
+
+v07_csv = './openrefine/v0.7/IHEC_metadata_harmonization.v0.7.csv'
+
+run([openrefine_client, '--export', f'--output={v07_csv}', old_project_name], check=True)
